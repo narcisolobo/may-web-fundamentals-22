2553361085dfa9f1759dcfb6e4a3ef93e3806f89
narcisolobo <narcisolobo@gmail.com>
2022-05-24 18:06:10 -0500

    added belt review part 2

---
+++ js_section/yellow_belt/script.js
@@ -0,0 +1,14 @@
+// changes banner image to pixel ninjas
+function nextImg(banner) {
+  banner.src = "./images/pixel-ninjas-2.png";
+}
+
+// changes banner image to stone punk
+function prevImg(banner) {
+  banner.src = "./images/stonepunk.png";
+}
+
+// increase cart item count
+function heart(count) {
+  count.innerText++;
+}
